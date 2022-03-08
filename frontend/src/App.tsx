39c2b881512b21b4c41c9e0af7941b4e70ea7ae7
jhonata padua <jhonatanps>
2022-03-07 22:22:46 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './components/Navbar';
+
 import './index.css';
+
+
 
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar />
   );
 }
 
